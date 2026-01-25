6dcd9f07c9db4e04a2135812cfbc25f7d13f41df
genaro <genaro@genaro240211>
2026-01-25 10:40:07 +0100

    Documenting bitstrings.

--- QREST/src/com/gnf/qrest/model/BitString.java
+++ QREST/src/com/gnf/qrest/model/BitString.java
@@ -8,36 +8,72 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * Models a BitString.
+ */
 public final class BitString implements Iterable<String> {
 
   private final List<String> strings;
 
+  /**
+   * Default constructor.
+   */
   public BitString() {
     strings = new ArrayList<String>();
   }
 
+  /**
+   * Constructor from list of strings.
+   *
+   * @param strings The list of strings.
+   */
   @JsonCreator
   public BitString(List<String> strings) {
     this.strings = List.copyOf(strings); // defensive copy
   }
 
+  /**
+   * Returns the bitstring as a list of strings.
+   *
+   * @return The list of strings.
+   */
   @JsonValue
   public List<String> asList() {
     return strings;
   }
 
+  /**
+   * Returns the size of the bitstring.
+   *
+   * @return The size of the bitstring.
+   */
   public int size() {
     return strings.size();
   }
 
+  /**
+   * Adds a string to the bitstring.
+   *
+   * @param string The string to addd.
+   */
   public void add(String string) {
     strings.add(string);
   }
 
+  /**
+   * Adds a collection of strings to the bitstring.
+   *
+   * @param strings The collection of strings to addd.
+   */
   public void addAll(Collection<String> strings) {
     this.strings.addAll(strings);
   }
 
+  /**
+   * Retrieves a string from the bitstring.
+   *
+   * @param index The index of the string.
+   */
   public String get(int index) {
     return strings.get(index);
   }
@@ -47,6 +83,11 @@ public final class BitString implements Iterable<String> {
     return strings.iterator();
   }
 
+  /**
+   * Returns the bitstring as a stream of strings.
+   *
+   * @return The list of strings.
+   */
   public Stream<String> stream() {
     return strings.stream();
   }
